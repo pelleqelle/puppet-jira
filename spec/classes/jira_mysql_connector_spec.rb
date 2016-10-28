f225651ca5272ee8ca8cf4b464dacfd73f0a5594
Pelle Kirkeby <pelle.kirkeby@gmail.com>
2016-10-28 18:05:04 -0500

    less aggro swap

--- spec/classes/jira_mysql_connector_spec.rb
+++ spec/classes/jira_mysql_connector_spec.rb
@@ -17,17 +17,17 @@ describe 'jira' do
                 mysql_connector_version: '5.1.34'
               }
             end
-            it { should contain_file('/opt/MySQL-connector').with_ensure('directory') }
+            it { expected.to contain_file('/opt/MySQL-connector').with_ensure('directory') }
             it do
-              should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/lib/mysql-connector-java.jar').
+              expected.to contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/lib/mysql-connector-java.jar').
                 with(
                   'ensure' => 'link',
                   'target' => '/opt/MySQL-connector/mysql-connector-java-5.1.34/mysql-connector-java-5.1.34-bin.jar'
                 )
             end
             it 'deploys mysql connector 5.1.34 from tar.gz' do
-              should contain_staging__file('mysql-connector-java-5.1.34.tar.gz').with('source' => 'https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.34.tar.gz')
-              should contain_staging__extract('mysql-connector-java-5.1.34.tar.gz').with('target' => '/opt/MySQL-connector',
+              expected.to contain_staging__file('mysql-connector-java-5.1.34.tar.gz').with('source' => 'https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.34.tar.gz')
+              expected.to contain_staging__extract('mysql-connector-java-5.1.34.tar.gz').with('target' => '/opt/MySQL-connector',
                                                                                          'creates' => '/opt/MySQL-connector/mysql-connector-java-5.1.34')
             end
           end
@@ -43,17 +43,17 @@ describe 'jira' do
                 mysql_connector_url: 'http://example.co.za/foo'
               }
             end
-            it { should contain_file('/opt/foo').with_ensure('directory') }
+            it { expected.to contain_file('/opt/foo').with_ensure('directory') }
             it do
-              should contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/lib/mysql-connector-java.jar').
+              expected.to contain_file('/opt/jira/atlassian-jira-6.3.4a-standalone/lib/mysql-connector-java.jar').
                 with(
                   'ensure' => 'link',
                   'target' => '/opt/foo/mysql-connector-java-5.1.15/mysql-connector-java-5.1.15-bin.jar'
                 )
             end
             it 'deploys mysql connector 5.1.15 from zip' do
-              should contain_staging__file('mysql-connector-java-5.1.15.zip').with('source' => 'http://example.co.za/foo/mysql-connector-java-5.1.15.zip')
-              should contain_staging__extract('mysql-connector-java-5.1.15.zip').with('target' => '/opt/foo',
+              expected.to contain_staging__file('mysql-connector-java-5.1.15.zip').with('source' => 'http://example.co.za/foo/mysql-connector-java-5.1.15.zip')
+              expected.to contain_staging__extract('mysql-connector-java-5.1.15.zip').with('target' => '/opt/foo',
                                                                                       'creates' => '/opt/foo/mysql-connector-java-5.1.15')
             end
           end
@@ -66,7 +66,7 @@ describe 'jira' do
                 mysql_connector_manage: false
               }
             end
-            it { should_not contain_class('jira::mysql_connector') }
+            it { expected.to_not contain_class('jira::mysql_connector') }
           end
         end
       end
